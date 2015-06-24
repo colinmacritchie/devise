e255e46fbd99c9ec6235784f2358e7714de8d777
Damien <damiennews@gmail.com>
2015-06-24 13:18:02 -0700

    Update confirmable.rb

--- lib/devise/models/confirmable.rb
+++ lib/devise/models/confirmable.rb
@@ -236,7 +236,7 @@ module Devise
         end
 
         def postpone_email_change?
-          postpone = self.class.reconfirmable && changes.has_key(:email)? && !@bypass_confirmation_postpone && self.email.present?
+          postpone = self.class.reconfirmable && changed.has_key(:email)? && !@bypass_confirmation_postpone && self.email.present?
           @bypass_confirmation_postpone = false
           postpone
         end
